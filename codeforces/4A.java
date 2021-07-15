dcae36f94db7c8f925df6d4d26a6545109400663
Programer171 <86323466+Programer171@users.noreply.github.com>
2021-07-15 15:40:40 -0700

    Created solution 4A

---
+++ codeforces/4A.java
@@ -0,0 +1,18 @@
+package Watermelon;
+
+import java.util.Scanner;
+
+public class Watermelon {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int input = scanner.nextInt();
+        if (input%2 == 0){
+            System.out.println("YES");
+            System.exit(0);
+        }else{
+            System.out.println("NO");
+            System.exit(0);
+        }
+    }
+}
