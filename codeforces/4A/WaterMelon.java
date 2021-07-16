aadfe164da65ac514d216438a85e40617f5479b1
Programer171 <86323466+Programer171@users.noreply.github.com>
2021-07-16 12:00:02 -0700

    Create WaterMelon.java

--- codeforces/4A/WaterMelon.java
+++ codeforces/4A/WaterMelon.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class waterMelon {
+public class WaterMelon {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
